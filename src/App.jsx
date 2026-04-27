0aa7f5263092c4526f297a7b00a0497fe805371a
gokaanil3 <gokaanil36@gmail.com>
2026-04-27 20:07:24 +0530

    update code and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom'
 import { useEffect, useMemo, useState } from 'react'
 import image1 from './assets/image1.jpeg'
+import day11 from './assets/Day 1-1.jpeg'
+import day12 from './assets/Day 1-2.jpeg'
+import day13 from './assets/Day 1-3.jpeg'
+import day14 from './assets/Day 1-4.jpeg'
+import day15 from './assets/Day 1-5.jpeg'
+import day21 from './assets/Day 2-1.jpeg'
+import day22 from './assets/Day 2-2.jpeg'
+import day23 from './assets/Day 2-3.jpeg'
+import day24 from './assets/Day 2-4.jpeg'
+import day25 from './assets/Day 2-5.jpeg'
 import './App.css'
 
 // Replace these URLs with your real custom photos anytime.
@@ -8,18 +18,18 @@ const HERO_IMAGE_URL = image1
 
 const CUSTOM_DAY_PHOTOS = [
   [
-    'src\assets\Day 1-1.jpeg',
-    'src\assets\Day 1-2.jpeg',
-    'src/assets/Day 1-3.jpeg',
-    'src/assets/Day 1-4.jpeg',
-    'src/assets/Day 1-5.jpeg',
+    day11,
+    day12,
+    day13,
+    day14,
+    day15,
   ],
   [
-    'src/assets/Day 2-1.jpeg',
-    'src/assets/Day 2-2.jpeg',
-    'src/assets/Day 2-3.jpeg',
-    'src/assets/Day 2-4.jpeg',
-    'src/assets/Day 2-5.jpeg',
+    day21,
+    day22,
+    day23,
+    day24,
+    day25,
   ],
   [
     'src/assets/Day 3-1.jpeg',
